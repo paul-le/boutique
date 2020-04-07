65717f00148f51d2dc538442511fe59f74a6ec0c
paul-le <paulloukyle@gmail.com>
2020-04-07 09:26:33 +0200

    Avancement projet

--- index.php
+++ index.php
@@ -1,1 +1,87 @@
-toast
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <section id="sectionTopHeader">
+            <h1 id="headerH1">Welcome</h1>
+        </section>
+        <section id="sectionBottomHeader">
+            <section id="imagesHeader">
+                <a href="index.php"><img id="bottomLogo" src="Images/logo.png"></a>
+                <a href="index.php"><img id="topLogo" src="Images/logov2.png"></a>
+            </section>
+            <section id="sectionBottomHeaderDroite">
+                <ul id="headerNavBar">
+                    <li>
+                        <img id="iconAccount" src="Images/iconacc.png">
+                        Mon compte
+                    </li>
+                    <li>
+                        <img id="iconAccount" src="Images/iconacc.png">
+                        Toast
+                    </li>
+                    
+                </ul>
+            </section>
+        </section>
+    </header>
+    <main>
+    <section id="flexFull">
+        <section id="partieGauche">
+            <!-- <img id="imgGaucheToast" src="Images/bannieregauche.jpg"> -->
+        </section>
+            <section id="partieCentreTop">
+                <section id="testTop">
+                    <img id="cartIcon" src="Images/carticonpng.png"> Mon panier
+                </section>
+                <section id="partieCentre">
+                    <img src="Images/banniere.jpg">
+                </section>
+                    <section id="mainContent">
+                        <section id="mainContentFlexSeparation">
+                            <section id="mainTopContent">
+                                <section id="mainTopContentCategories">
+                                    DERNIERS COMMENTAIRES
+                                </section>
+                                <section id="mainTopContentListe">
+                                    DERNIERS JEUX AJOUTÉS
+                                </section>
+                                <section id="mainTopContentLastComment">
+                                    CATÉGORIES
+                                </section>
+                        </section>
+                        <section id="banniereSeparation">
+
+                            </section>
+                            <section id="meilleuresVentes">
+                                Meilleurs ventes
+                            </section>
+                        </section>
+                    </section>
+                </section>
+        <section id="partieDroite">
+            <!-- <img src="Images/bannieredroite.jpg"> -->
+        </section>
+    </section>
+    </main>
+    <footer>
+        <section id="footerMainSection">
+            <section id="footerFirstSection">
+                copyright Momohamed Paul
+            </section>
+            <section id="footerSecondSection">
+                Accueil Nous contacter FAQ
+            </section>
+            <section id="footerThirdSection">
+                <img src="Images/paiement.png">
+            </section>
+        </section>
+    </footer>
+</body>
+</html>
