959f8e2a15d85d216fdfaad98b4d4c2018f9d914
paul-le <paulloukyle@gmail.com>
2020-04-10 19:31:43 +0200

    CSS d'admin , avancement produits.php

--- produits.php
+++ produits.php
@@ -1,7 +1,15 @@
 <?php
 
     session_start();
+    include('fonctions.php');
     var_dump($_SESSION);
+    ob_start();
+    $getIdProduit = $_GET['id'];
+    $connexion = mysqli_connect('Localhost','root','','boutique');
+    $requeteDonneesProduits = "SELECT * FROM produits WHERE id=\"$getIdProduit\"";
+    $queryDonneesProduits = mysqli_query($connexion,$requeteDonneesProduits);
+    $resultatDonneesProduits = mysqli_fetch_all($queryDonneesProduits);
+    var_dump($resultatDonneesProduits);
 
 ?>
 
@@ -28,17 +36,17 @@
                 </section>
                     <section id="partieCentreProduits">
                         <section id="partieCentreProduitsGauche">
-                            IMAGE
+                            <img src="imgArticle/<?php echo $resultatDonneesProduits[0][7] ?>">
                         </section>
                         <section id="partieCentreProduitsDroite">
                             <section class="nomDuProduit">
-                                Nom du produit
+                                <?php echo $resultatDonneesProduits[0][3]; ?>
                             </section>
                             <section class="descDuProduit">
-                                Description du produit
+                                <?php echo $resultatDonneesProduits[0][4]; ?>
                             </section>
                             <section class="prixDuProduit">
-                                Prix du produit & Quantité & Ajouter au panier
+                                <?php echo "".$resultatDonneesProduits[0][5]."€"; ?> <br> <?php echo "Quantité : ".$resultatDonneesProduits[0][6]."" ?> <br> Ajouter au panier
                             </section>
                         </section>
                     </section>
@@ -47,8 +55,64 @@
                             Commentaires des utilisateurs :
                         </section>
                         <section id="commentaireSurLeProduit">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quibusdam veritatis vitae molestiae perspiciatis asperiores, odio autem placeat. Alias pariatur praesentium illum ut dolore voluptatibus ipsam eos corrupti porro voluptas!
-                        </section>  
+                        <?php 
+
+                            // $requeteListeCom = "SELECT * FROM commentaires WHERE id_produit=\"$getIdProduit\" ORDER BY id DESC LIMIT 5";
+                            $requeteListeCom = "SELECT c.*,u.login FROM commentaires AS c INNER JOIN utilisateurs AS u ON c.id_utilisateur WHERE id_produit=\"$getIdProduit\" AND u.id = c.id_utilisateur ORDER BY c.id DESC LIMIT 5";
+                            $queryListeCom = mysqli_query($connexion,$requeteListeCom);
+                            $resultatListeCom = mysqli_fetch_all($queryListeCom);
+                            // var_dump($resultatListeCom);
+                            $i = 0;
+
+                            while($i <= 5)
+                            { ?> <section id="commentaireSurLeProduit"> <?php
+                                echo "".$resultatListeCom[$i][6]." ".$resultatListeCom[$i][3]." ".$resultatListeCom[$i][5]."<br><br>";
+                                $i++; ?> 
+                                </section> 
+                            <?php
+                            }
+                                
+                        ?>
+                        
+                        <section id="commentaireFormTopBar">
+                            Écrire un commentaire :
+                        </section>
+                        <section id="commentaireFormProduitSection">
+                            <form id="formToast" action="" method="POST">
+                                <select type="post" name="noteProduit">
+                                    <option>Votre note</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select>
+                                <textarea id="textAreaCommentaire" name="commentaireProduit" rows="7" cols="50" placeholder="Votre commentaire"></textarea>
+                                <input type="submit" name="envoyerCommentaire" value="Envoyer">
+                            </form>
+                            <?php
+                                if(isset($_POST['envoyerCommentaire']))
+                                {
+                                    if(strlen($_POST['envoyerCommentaire']) != 0 && $_POST['noteProduit'] != "Votre note")
+                                    {
+                                        $idSession = $_SESSION['id'];
+                                        $idProduit = $resultatDonneesProduits[0][0];
+                                        $postCom = $_POST['commentaireProduit'];
+                                        $postNote = $_POST['noteProduit'];
+                                        $insertDate = date("Y-m-d H:i:s", strtotime("now-2"));
+                                        $requeteInsertCommentaire = "INSERT INTO commentaires (id_utilisateur,id_produit,commentaire,note,date) VALUES('$idSession','$idProduit','$postCom','$postNote','$insertDate')";
+                                        $queryInsertCommentaire = mysqli_query($connexion,$requeteInsertCommentaire);
+                                        header('Location:produits.php?id='.$getIdProduit.'');
+                                    }
+                                    else
+                                    {
+                                    echo "<br><br><br>Veuillez remplir le champ et choisir une note !";
+                                    }
+                                }
+                                
+
+                                ?>
+                        </section>
                     </section>
         <!-- <section id="partieDroiteProduits">
         </section> -->
@@ -56,7 +120,7 @@
     </main>
         <?php
     include('footer.php');
-
+    ob_end_flush();
     ?>
 </body>
 </html>
